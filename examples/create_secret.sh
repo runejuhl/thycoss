b8925dd4b9b0300c5c19a75a8c10a5d056d292ec
Rune Juhl Jacobsen <runejuhl@petardo.dk>
2020-02-18 10:42:29 +0100

    Add example script for creating a new secret

---
+++ examples/create_secret.sh
@@ -0,0 +1,55 @@
+#!/usr/bin/env bash
+#
+# Create a new secret
+
+set -euo pipefail
+
+# shellcheck source=../thycoss.sh
+. "$(dirname "$(readlink -f "$0")")/../thycoss.sh"
+
+folder_id=71
+user=root
+hostname=someserver.contoso.com
+_curl_form post '/api/v1/secrets' <<EOF
+{
+  "name": "${user}@${hostname}",
+  "secretTemplateId": 6007,
+  "folderId": ${folder_id},
+  "siteId": 1,
+  "checkOutEnabled": false,
+  "autoChangeEnabled": false,
+  "enableInheritPermissions": true,
+  "enableInheritSecretPolicy": true,
+  "items": [
+   {
+      "itemValue": "${hostname}",
+      "fieldId": 108,
+      "fieldName": "Machine",
+      "slug": "machine",
+      "fieldDescription": "The Server or Location of the Unix Machine.",
+      "isFile": false,
+      "isNotes": false,
+      "isPassword": false
+    },
+    {
+      "itemValue": "${user}",
+      "fieldId": 111,
+      "fieldName": "Username",
+      "slug": "username",
+      "fieldDescription": "The Unix Machine Username.",
+      "isFile": false,
+      "isNotes": false,
+      "isPassword": false
+    },
+    {
+      "itemValue": "this here be real secret",
+      "fieldId": 110,
+      "fieldName": "Password",
+      "slug": "password",
+      "fieldDescription": "The password of the Unix Machine.",
+      "isFile": false,
+      "isNotes": false,
+      "isPassword": true
+    }
+  ]
+}
